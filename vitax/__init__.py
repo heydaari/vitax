a8532ba7941d28b6a71abae12b39383b0d8562c9
Mohammad Hasan Heydari <heidary0081@gmail.com>
2025-08-25 23:47:41 +0330

    version upgrade

--- vitax/__init__.py
+++ vitax/__init__.py
@@ -1,9 +1,7 @@
-from vitax.models import VisionTransformer
-from vitax.models import TransformerEncoder
-
+from vitax.models.vit import VisionTransformer
 from vitax.training import compute_losses_and_logits
 from vitax.training import eval_step
 from vitax.training import train_step
-from vitax.training import train_one_epoch
 
-__version__ = "0.0.1"
+
+__version__ = "0.0.3"
